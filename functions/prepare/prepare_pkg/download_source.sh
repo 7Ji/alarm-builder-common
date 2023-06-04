ce732663a14820d57346e4552e81b9ad6d5946e4
7Ji <pugokushin@gmail.com>
2023-06-04 20:51:39 +0800

    functions/prepare/prepare_pkg/download_source: fix place to set missing dir_pkg_absolute

--- functions/prepare/prepare_pkg/download_source.sh
+++ functions/prepare/prepare_pkg/download_source.sh
@@ -5,12 +5,12 @@ download_source() {
   local SRCDEST=.
   local lib=
   (
+    local dir_pkg_absolute="$(readlink -f "${dir_pkg}")"
     for lib in "$LIBRARY"/*.sh; do
       source "$lib"
     done
     load_makepkg_config
     pushd "${dir_build}" > /dev/null
-    local dir_pkg_absolute="$(readlink -f "${dir_pkg}")"
     local build_pkg=
     for build_pkg in *; do
       if [[ -d "${build_pkg}" ]]; then
